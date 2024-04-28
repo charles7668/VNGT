6c990b45a221974a88979c1df00d40b79cf919cb
charles7668 <charles76680119@gmail.com>
2024-04-28 17:53:02 +0800

    [feat] Add Dependency Injection

--- SavePatcher/Program.cs
+++ SavePatcher/Program.cs
@@ -1,13 +1,28 @@
+using Microsoft.Extensions.DependencyInjection;
+using SavePatcher.Extractor;
+using System.Diagnostics;
+
 namespace SavePatcher
 {
     internal static class Program
     {
+        /// <summary>
+        /// Service provider for program.
+        /// </summary>
+        public static IServiceProvider ServiceProvider { get; private set; } = null!;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         private static void Main()
         {
+            // register services
+            ServiceCollection services = new();
+            services.AddSingleton<ExtractorFactory>();
+            ServiceProvider = services.BuildServiceProvider();
+            Debug.Assert(ServiceProvider != null);
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
