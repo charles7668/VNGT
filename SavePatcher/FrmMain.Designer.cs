5ff711960b34a36e94f25c51cb358806ccb5cb07
charles <charles76680119@gmail.com>
2024-04-29 16:40:49 +0800

    [UI (SavePatcher)] modify ui

--- SavePatcher/FrmMain.Designer.cs
+++ SavePatcher/FrmMain.Designer.cs
@@ -30,6 +30,7 @@
         {
             cmbConfigList = new ComboBox();
             btnPatch = new Button();
+            txtLog = new TextBox();
             SuspendLayout();
             // 
             // cmbConfigList
@@ -51,22 +52,33 @@
             btnPatch.Text = "Patch";
             btnPatch.UseVisualStyleBackColor = true;
             // 
+            // txtLog
+            // 
+            txtLog.Location = new Point(30, 104);
+            txtLog.Multiline = true;
+            txtLog.Name = "txtLog";
+            txtLog.Size = new Size(736, 248);
+            txtLog.TabIndex = 2;
+            // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(812, 387);
+            Controls.Add(txtLog);
             Controls.Add(btnPatch);
             Controls.Add(cmbConfigList);
             Name = "FrmMain";
             Text = "Save Patcher";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ComboBox cmbConfigList;
         private Button btnPatch;
+        private TextBox txtLog;
     }
 }
