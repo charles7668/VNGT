264433f97855669dd21a989dc454c27c68e4fd6e
charles <charles76680119@gmail.com>
2024-04-30 11:29:43 +0800

    [feat (Helper)] Add string extension for check path is http link

---
+++ Helper/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Helper
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// check link is http or https
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>true if input is http link</returns>
+        public static bool IsHttpLink(this string input)
+        {
+            return input.StartsWith("http://") || input.StartsWith("https://");
+        }
+    }
+}
