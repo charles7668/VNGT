cacb15f771c3c013f178fce6b2fe3967883c928c
charles <charles76680119@gmail.com>
2024-04-30 10:49:40 +0800

    [feat (Helper)] Add FileDownloader

---
+++ Helper/Web/FileDownloader.cs
@@ -0,0 +1,26 @@
+﻿namespace Helper.Web
+{
+    public static class FileDownloader
+    {
+        private static readonly HttpClient _Client = new();
+
+        public static async Task<(bool success, string errorMessage)> DownloadFileAsync(string fileUrl,
+            string destinationPath)
+        {
+            try
+            {
+                using var response = await _Client.GetAsync(fileUrl, HttpCompletionOption.ResponseHeadersRead);
+                await using var streamToReadFrom = await response.Content.ReadAsStreamAsync();
+                string fileToWriteTo = Path.GetFullPath(destinationPath);
+                await using Stream streamToWriteTo = File.Open(fileToWriteTo, FileMode.Create);
+                await streamToReadFrom.CopyToAsync(streamToWriteTo);
+
+                return (true, string.Empty);
+            }
+            catch (Exception e)
+            {
+                return (false, $"Error downloading file: {e.Message}");
+            }
+        }
+    }
+}
