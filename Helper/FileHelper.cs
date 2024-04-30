7869657023075e65ac9eba8932737cbb5e7bad1d
charles <charles76680119@gmail.com>
2024-04-30 09:49:21 +0800

    [feat (Helper)] Add helper project and FileHelper class

---
+++ Helper/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace Helper
+{
+    public static class FileHelper
+    {
+        /// <summary>
+        /// copy all file in source directory to destination directory
+        /// </summary>
+        /// <param name="sourceDir"></param>
+        /// <param name="destinationDir"></param>
+        public static void CopyDirectory(string sourceDir, string destinationDir)
+        {
+            Directory.CreateDirectory(destinationDir);
+
+            foreach (string file in Directory.GetFiles(sourceDir))
+            {
+                string destFile = Path.Combine(destinationDir, Path.GetFileName(file));
+                File.Copy(file, destFile, true);
+            }
+
+            foreach (string directory in Directory.GetDirectories(sourceDir))
+            {
+                string destDir = Path.Combine(destinationDir, Path.GetFileName(directory));
+                CopyDirectory(directory, destDir);
+            }
+        }
+    }
+}
