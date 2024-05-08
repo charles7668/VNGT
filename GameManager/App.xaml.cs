59ab2eafb60d947e774c0730a1abf56d4260083d
charles <charles76680119@gmail.com>
2024-05-08 09:49:28 +0800

    [build (GameManager)] Add GameManager project

---
+++ GameManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GameManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ GameManager/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GameManager
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
