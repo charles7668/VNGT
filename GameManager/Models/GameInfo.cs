e1adcbe02d709a7d878ee8e3d53a5c164184f126
charles7668 <charles76680119@gmail.com>
2024-05-08 20:53:30 +0800

    [feat (GameManager)] Adjust card component UI.

--- GameManager/Models/GameInfo.cs
+++ GameManager/Models/GameInfo.cs
@@ -7,5 +7,7 @@
         public string ExePath { get; set; } = string.Empty;
 
         public bool Display { get; set; } = true;
+
+        public string CoverPath { get; set; } = string.Empty;
     }
 }
