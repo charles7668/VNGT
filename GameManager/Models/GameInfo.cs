e8b5d3df05a26216860306540cc40822f6418950
charles <charles76680119@gmail.com>
2024-05-08 17:16:08 +0800

    [feat (GameManager)] Add Search.

---
+++ GameManager/Models/GameInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace GameManager.Models
+{
+    public class GameInfo
+    {
+        public string GameName { get; set; } = string.Empty;
+
+        public string ExePath { get; set; } = string.Empty;
+
+        public bool Display { get; set; } = true;
+    }
+}
