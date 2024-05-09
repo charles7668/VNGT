97e28bbbc53c3885c1c167d8f5c19cc0b91a6de6
charles7668 <charles76680119@gmail.com>
2024-05-10 07:28:15 +0800

    [feat (GameManager)] Add dialog for edit game info

---
+++ Helper/Image/ImageHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Helper.Image
+{
+    public static class ImageHelper
+    {
+        public static string? ImageFileToBase64(string? filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                return null;
+            }
+
+            if (!File.Exists(filePath))
+                return null;
+
+            byte[] imageArray = File.ReadAllBytes(filePath);
+            return "data:image/png;base64, " + Convert.ToBase64String(imageArray);
+        }
+    }
+}
