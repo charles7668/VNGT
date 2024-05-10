a6052985f6b872ad69dccf124364d65409b7bed6
charles <charles76680119@gmail.com>
2024-05-10 10:44:22 +0800

    [feat (GameManager)] Added "Add New Game Info" feature.

---
+++ GameManager/DataMapService.cs
@@ -0,0 +1,24 @@
+﻿using GameManager.Components.Pages.components;
+using GameManager.Models;
+
+namespace GameManager
+{
+    public static class DataMapService
+    {
+        public static void Map(DialogGameInfoEdit.FormModel src, GameInfo dest)
+        {
+            dest.CoverPath = src.Cover;
+            dest.GameName = src.GameName;
+            dest.ExePath = src.ExePath;
+            dest.DateTime = src.DateTime;
+        }
+
+        public static void Map(GameInfo src, DialogGameInfoEdit.FormModel dest)
+        {
+            dest.Cover = src.CoverPath;
+            dest.GameName = src.GameName;
+            dest.ExePath = src.ExePath;
+            dest.DateTime = src.DateTime;
+        }
+    }
+}
--- GameManager/Models/GameInfo.cs
+++ GameManager/Models/GameInfo.cs
@@ -2,12 +2,14 @@
 {
     public class GameInfo
     {
-        public string GameName { get; set; } = string.Empty;
+        public string? GameName { get; set; } = string.Empty;
 
-        public string ExePath { get; set; } = string.Empty;
+        public string? ExePath { get; set; } = string.Empty;
 
         public bool Display { get; set; } = true;
 
-        public string CoverPath { get; set; } = string.Empty;
+        public string? CoverPath { get; set; } = string.Empty;
+
+        public DateTime? DateTime { get; set; }
     }
 }
