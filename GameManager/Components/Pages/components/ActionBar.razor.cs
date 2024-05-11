ce7ece13201ee40d6a1c7f00c3341e5d94f06e1f
charles7668 <charles76680119@gmail.com>
2024-05-11 15:37:36 +0800

    [refactor (GameManager)] Move ActionBar @code part to single cs file

---
+++ GameManager/Components/Pages/components/ActionBar.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GameManager.Components.Pages.components
+{
+    public partial class ActionBar
+    {
+        private string? SearchText { get; set; }
+
+        [Parameter]
+        public EventCallback<string> AddNewGameEvent { get; set; }
+
+        [Parameter]
+        public EventCallback<string?> SearchEvent { get; set; }
+
+        private async Task OnAddNewGame()
+        {
+            var customFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                { DevicePlatform.WinUI, new[] { ".exe" } }
+            });
+
+            var options = new PickOptions
+            {
+                PickerTitle = "Please select game exe file",
+                FileTypes = customFileType
+            };
+
+            FileResult? result = await FilePicker.PickAsync(options);
+            if (result == null)
+            {
+                return;
+            }
+
+            if (AddNewGameEvent.HasDelegate)
+            {
+                await AddNewGameEvent.InvokeAsync(result.FullPath);
+            }
+        }
+
+        private async Task OnSearch()
+        {
+            if (SearchEvent.HasDelegate)
+            {
+                await SearchEvent.InvokeAsync(SearchText);
+            }
+        }
+    }
+}
