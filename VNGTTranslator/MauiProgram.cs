9ba0429912358fc368e00c302e953e1957116e6a
charles <charles76680119@gmail.com>
2024-06-05 15:42:50 +0800

    [Feat] Add MudBlazor to translator project

--- VNGTTranslator/MauiProgram.cs
+++ VNGTTranslator/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace VNGTTranslator
 {
@@ -15,6 +16,7 @@ namespace VNGTTranslator
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
 
 #if DEBUG
     		builder.Services.AddBlazorWebViewDeveloperTools();
