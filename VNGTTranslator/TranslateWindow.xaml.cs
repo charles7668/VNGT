72f190885cfc0b6f9e44c45cc2d7a20d8ecf06c6
charles7668 <charles76680119@gmail.com>
2024-06-06 19:46:43 +0800

    [Feat] Add AllowWindowChangeSize

--- VNGTTranslator/TranslateWindow.xaml.cs
+++ VNGTTranslator/TranslateWindow.xaml.cs
@@ -12,26 +12,48 @@ namespace VNGTTranslator
         public TranslateWindow()
         {
             InitializeComponent();
+            SetBackgroundTransparent(false);
+        }
+
+        private readonly string _backgroundColor = "#7F000000";
+
+        /// <summary>
+        /// set background transparent or not
+        /// </summary>
+        /// <param name="isTransparent"></param>
+        private void SetBackgroundTransparent(bool isTransparent)
+        {
+            Background = isTransparent
+                ? new SolidColorBrush(Colors.Transparent)
+                : new SolidColorBrush((Color)ColorConverter.ConvertFromString(_backgroundColor));
+        }
+
+        /// <summary>
+        /// Set allow change size or not
+        /// </summary>
+        /// <param name="isAllow"></param>
+        private void SetAllowChangeSize(bool isAllow)
+        {
+            if (isAllow)
+            {
+                BorderThickness = new Thickness(3);
+                ResizeMode = ResizeMode.CanResizeWithGrip;
+            }
+            else
+            {
+                BorderThickness = new Thickness(0);
+                ResizeMode = ResizeMode.NoResize;
+            }
         }
 
         private void BtnLock_OnClick(object sender, RoutedEventArgs e)
         {
-            if (!(((ToggleButton)sender).IsChecked ?? false))
-            {
-                Background = new SolidColorBrush(Colors.Transparent);
-            }
-            else
-            {
-                BrushConverter brushConverter = new();
-                var brush = (Brush?)brushConverter.ConvertFromString("#7F000000");
-                if (brush != null)
-                    Background = brush;
-            }
+            SetBackgroundTransparent(((ToggleButton)sender).IsChecked ?? false);
         }
 
         private void BtnAllowSizeChange_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            SetAllowChangeSize(((ToggleButton)sender).IsChecked ?? false);
         }
 
         private void BtnPause_OnClick(object sender, RoutedEventArgs e)
@@ -66,12 +88,12 @@ namespace VNGTTranslator
 
         private void BtnMinimizeWindow_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            WindowState = WindowState.Minimized;
         }
 
         private void BtnExit_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Close();
         }
     }
 }
