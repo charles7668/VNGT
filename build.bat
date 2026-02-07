c683de821ad11c227dd5adb01b3335d00c5eb0e2
charles7668 <charles76680119@gmail.com>
2026-02-07 19:40:36 +0800

    build: Update requirements to .Net10

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal
 
-dotnet publish GameManager -f net8.0-windows10.0.19041.0 -o "./bin/GameManager x86" -c Release -r win10-x86 -p:WindowsPackageType=None
+dotnet publish GameManager -f net10.0-windows10.0.19041.0 -o "./bin/GameManager x86" -c Release -r win-x86 -p:WindowsPackageType=None
 dotnet publish SavePatcher -c Release -o "./bin/SavePatcher x86" -r win-x86
-dotnet publish GameManager -f net8.0-windows10.0.19041.0 -o "./bin/GameManager" -c Release -r win10-x64 -p:WindowsPackageType=None
+dotnet publish GameManager -f net10.0-windows10.0.19041.0 -o "./bin/GameManager" -c Release -r win-x64 -p:WindowsPackageType=None
 dotnet publish SavePatcher -c Release -o "./bin/SavePatcher x64" -r win-x64
 
 echo start build ProcessTracer...
